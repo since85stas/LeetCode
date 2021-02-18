ec027f3c1d85af4a88ed5fc55ff03af3a3efd9cd
home <seeyou85@gmail.com>
2021-02-18 13:32:12 +0300

    добавлен Calculator

---
+++ src/yandAlgh/sprint2/Calculator.java
@@ -0,0 +1,52 @@
+package yandAlgh.sprint2;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Calculator {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("input.txt"))));
+
+        StringTokenizer tokenizerStr = new StringTokenizer(reader.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+
+        while (tokenizerStr.hasMoreTokens()) {
+            String elem = tokenizerStr.nextToken();
+            if (elem.equals("+")) {
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+
+                int sum = num1 + num2;
+                stack.add(sum);
+            } else if (elem.equals("-")) {
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+
+                int minus = num2 - num1;
+                stack.add(minus);
+            } else if (elem.equals("*")) {
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+
+                int mult = num1 * num2;
+                stack.add(mult);
+            } else if (elem.equals("/")) {
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+
+                double del = (float) num2 / (float) num1;
+                double res = Math.floor(del);
+                stack.add((int) res);
+            } else {
+                stack.add(Integer.parseInt(elem));
+            }
+//            System.out.println(elem);
+        }
+
+        System.out.println(stack.pop());
+    }
+
+}
