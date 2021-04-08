4d23ee5a19895b70f52aedc4581884599b876c27
home <seeyou85@gmail.com>
2021-04-08 12:09:30 +0400

    case G

--- src/yandAlgh/sprint5/Solution.java
+++ src/yandAlgh/sprint5/Solution.java
@@ -1,12 +1,10 @@
-//package yandAlgh.sprint5;
+package yandAlgh.sprint5;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
 
-//    static int lastPos = 1;
-//
 //    public static int siftUp(int[] heap, int idx) {
 //
 //        if (idx == 1) {
@@ -18,15 +16,38 @@ public class Solution {
 //            int temp = heap[parent];
 //            heap[parent] = heap[idx];
 //            heap[idx] = temp;
-//            lastPos = siftUp(heap, parent);
+//            idx = siftUp(heap, parent);
 //        }
-//        return lastPos;
+//        return idx;
 //    }
 
-    public static boolean treeSolution(Node head) {
+    public static int siftDown(int[] heap, int idx) {
+        // your code
+        int left = 2 * idx;
+        int right = 2 * idx + 1;
+        int index_largest = idx;
+        if (heap.length <= left || heap.length == 2) {
+            return idx;
+        }
+        if ( right < heap.length && heap[left] < heap[right]) {
+            index_largest = right;
+        } else {
+            index_largest = left;
+        }
 
-        return anagr(head);
+        if (heap[idx] < heap[index_largest]) {
+            int temp = heap[index_largest];
+            heap[index_largest] = heap[idx];
+            heap[idx] = temp;
+            idx = siftDown(heap, index_largest);
+        }
+        return idx;
     }
+
+//    public static boolean treeSolution(Node head) {
+//
+//        return anagr(head);
+//    }
 
     /* Возвращает true, если двоичное дерево с корнем в качестве корня сбалансировано по высоте */
 
@@ -50,46 +71,46 @@ public class Solution {
 //        /* Если мы достигаем здесь, то дерево не сбалансировано по высоте */
 //        return Math.max(lh, rh);
 //    }
-    static List<Integer> lefta = new ArrayList<>();
-    static List<Integer> righta = new ArrayList<>();
-
-    static boolean anagr(Node head) {
-        if (head.left != null) LMR(head.left);
-        if (head.right != null) RMR(head.right);
-        return lefta.equals(righta);
-    }
-
-    static void LMR(Node node) {
-        if (node.left != null) LMR(node.left);
-//        System.out.println(node.value);
-        lefta.add(node.value);
-        if (node.right != null) LMR(node.right);
-    }
-
-    static void RMR(Node node) {
-        if (node.right != null) RMR(node.right);
-//        System.out.println(node.value);
-        righta.add(node.value);
-        if (node.left != null) RMR(node.left);
-    }
+//    static List<Integer> lefta = new ArrayList<>();
+//    static List<Integer> righta = new ArrayList<>();
+//
+//    static boolean anagr(Node head) {
+//        if (head.left != null) LMR(head.left);
+//        if (head.right != null) RMR(head.right);
+//        return lefta.equals(righta);
+//    }
+//
+//    static void LMR(Node node) {
+//        if (node.left != null) LMR(node.left);
+////        System.out.println(node.value);
+//        lefta.add(node.value);
+//        if (node.right != null) LMR(node.right);
+//    }
+//
+//    static void RMR(Node node) {
+//        if (node.right != null) RMR(node.right);
+////        System.out.println(node.value);
+//        righta.add(node.value);
+//        if (node.left != null) RMR(node.left);
+//    }
 
     /* ПОЛЕЗНЫЕ ФУНКЦИИ ДЛЯ ТЕСТИРОВАНИЯ isBalanced () ФУНКЦИЯ */
             /* Функция вычисляет «высоту» дерева. Высота это
     количество узлов вдоль самого длинного пути от корневого узла
     вплоть до самого дальнего листового узла. */
-    static int height(Node node)
-    {
-        /* базовое дерево пусто */
-        if (node == null)
-            return 0;
-
-        int hl = height(node.left);
-        int hr = height(node.right);
-        /* Если дерево не пустое, то высота = 1 + максимум слева
-        высота и правая высота */
-        return 1 + Math.max(hl, hr);
-
-    }
+//    static int height(Node node)
+//    {
+//        /* базовое дерево пусто */
+//        if (node == null)
+//            return 0;
+//
+//        int hl = height(node.left);
+//        int hr = height(node.right);
+//        /* Если дерево не пустое, то высота = 1 + максимум слева
+//        высота и правая высота */
+//        return 1 + Math.max(hl, hr);
+//
+//    }
 
 
 //    public static class Node {
--- src/yandAlgh/sprint5/SolutionTest.java
+++ src/yandAlgh/sprint5/SolutionTest.java
@@ -2,6 +2,8 @@ package yandAlgh.sprint5;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
@@ -9,27 +11,32 @@ class SolutionTest {
     @Test
     void treeSolution() {
 
-        Solution.Node head = new Solution.Node(1);
-
-        head.left = new Solution.Node(2);
-
-        head.right = new Solution.Node(2);
-
-        head.left.left = new Solution.Node(3);
-
-        head.left.right = new Solution.Node(4);
-
-        head.right.left = new Solution.Node(3);
-
-        head.right.right = new Solution.Node(4);
-
-        boolean res =Solution.treeSolution(head);
+//        Solution.Node head = new Solution.Node(1);
+//
+//        head.left = new Solution.Node(2);
+//
+//        head.right = new Solution.Node(2);
+//
+//        head.left.left = new Solution.Node(3);
+//
+//        head.left.right = new Solution.Node(4);
+//
+//        head.right.left = new Solution.Node(3);
+//
+//        head.right.right = new Solution.Node(4);
+//
+//        boolean res =Solution.treeSolution(head);
 
 //        int[] arr = new int[] {0, 12, 6, 8,3,15,7};
-//
-//        int res = Solution.siftUp(arr, 5);
+
+//        int[] arr = new int[] {0,12, 1, 8,3,4,7};
+
+        int[] arr = new int[] {0,1,2,3};
+
+        int res = Solution.siftDown(arr, 2);
 
         System.out.println(res);
+        System.out.println(Arrays.toString(arr));
 
     }
 }
