74b24724f850864e0ea6519a07af10a73fc9821d
Note <seeyou85@gmail.com>
2021-04-15 12:28:13 +0300

    решение прошло, рефакторинг

--- src/yandAlgh/sprint5/Solution.java
+++ src/yandAlgh/sprint5/Solution.java
@@ -1,23 +1,38 @@
 package yandAlgh.sprint5;
 
+/**
+ * Сам алгорим описан в теории. Сначала рассматриваем крайние случаи с вырожденными деревьями.
+ * Затем, ищем замену для узла который надо удалить. Либо мамый правый элемент в левом поддереве, либо
+ * если его нет то самый левый в правом поддереве. Заменяем на него удаляемый и если у заменяемого остались
+ * дети то прибвляем его к родителю замены.
+ *
+ * сложность алгоритма O(k) - глубины дерева
+ *
+ * успешная посылка https://contest.yandex.ru/contest/23815/run-report/49289745/
+ */
 public class Solution {
 
-    static Node lastParent;
+    // ссылка на родителя для удаляемого
+    static Node delParent;
+
+    static Node changeParent;
 
     public static Node remove(Node root, int key) {
 
+        // если ничего нет то возвращаем null
         if (root == null) return null;
 
         // находим узел для удаления
-        Node toDel = find(root, lastParent, key);
+        Node toDel = find(root, delParent, key);
 
         // если такого узла в дереве нет то ничего не меняем
         if (toDel == null) return root;
 
+        // если удаляемый это вершина и потомком нет то вернем null
         if (toDel.equals(root) && toDel.getLeft() == null && toDel.getRight() == null) return null;
 
-        // находим родителя узла
-        Node delParent = lastParent;
+        // родителя удаляемого узла
+        Node delParent = Solution.delParent;
 
         // если у удаляемого нет потомков, значит мы можем просто удалить его, для этого у родителя заменяем его ссылку на null
         if (delParent != null && toDel.getLeft() == null && toDel.getRight() == null) {
@@ -28,12 +43,18 @@ public class Solution {
 
         // в противном случае ищем ему замену
         // находим замену узлу
-        if (toDel.getLeft() != null && toDel.getRight() != null) {
-                Node change = findChange(toDel, key);
+        if (toDel.getLeft() != null) {
+
+                // находим замену
+                Node change = findChange(toDel, changeParent);
 
                 // находим родителя замены
-                Node changeParent = findParent(root, change.getValue());
+                Node changeParent = Solution.changeParent;
+//                        = findParent(root, change.getValue());
 
+                // если получится что удаляемый элемент является родителем замены
+                // то в таком случае можем просто переопределить левое поддерево замены удаляемому
+                // все потомки будут там
                 if (changeParent == toDel) {
                     toDel.setValue(change.getValue());
                     toDel.setLeft(change.getLeft());
@@ -41,44 +62,63 @@ public class Solution {
                     // если у замены нет потомков то просто заменяем
                     if (change.getLeft() == null) {
                         changeParent.setRight(null);
+
+                        // меняем згачение
                         toDel.setValue(change.getValue());
                     } else {
+                        // если они есть, то вставляем их в правую ветвь родителя
                         changeParent.setRight(change.getLeft());
+
+                        // меняем згачение
                         toDel.setValue(change.getValue());
                     }
                 }
+        } else if (toDel.getRight() != null) {
+            Node change = findChange(toDel, changeParent);
 
-        } else if (toDel.getLeft() != null || toDel.getRight() != null) {
-            if (delParent != null) {
-                if (toDel.getLeft() != null) {
-                    delParent.setLeft(toDel.getLeft());
-                } else {
-                    delParent.setRight(toDel.getRight());
-                }
+            // находим родителя замены
+            Node changeParent = Solution.changeParent;
+//                    = findParent(root, change.getValue());
+
+            if (changeParent == toDel) {
+                toDel.setValue(change.getValue());
+                toDel.setRight(change.getRight());
             } else {
-                if (toDel.getLeft() != null) {
-                    root = toDel.getLeft();
+                // если у замены нет потомков то просто заменяем
+                if (change.getRight() == null) {
+                    changeParent.setLeft(null);
+
+                    // меняем згачение
+                    toDel.setValue(change.getValue());
                 } else {
-                    root = toDel.getRight();
+                    changeParent.setLeft(change.getRight());
+
+                    // меняем згачение
+                    toDel.setValue(change.getValue());
                 }
             }
         }
-
         return root;
-
     }
 
+    /**
+     * Здесь не знаю может есть более эллегантный способ получить еще и родителя вершино,
+     * но я сделал через статичепское поле класса, которое указывает на след элемент
+     * @param root ссылка на вершину
+     * @param parent ссылка на родителя вершины
+     * @param key ключ который ищем
+     */
     private static Node find(Node root, Node parent, int key) {
         if (root.getValue() == key) {
-            lastParent = parent;
+            delParent = parent;
             return root;
         }
         if (root.getLeft() != null && key < root.getValue()) {
-            lastParent = parent;
+            delParent = parent;
             return find(root.getLeft(), root, key);
         }
         if (root.getRight() != null && key >= root.getValue()) {
-            lastParent = parent;
+            delParent = parent;
             return find(root.getRight(), root, key);
         }
         return null;
@@ -99,17 +139,30 @@ public class Solution {
         return null;
     }
 
-    private static Node findChange(Node toDel, int key) {
-       return getRightest(toDel.getLeft());
+    private static Node findChange(Node toDel, Node parent) {
+       if (toDel.getLeft() != null) {
+//           changeParent = toDel;
+           return getRightest(toDel.getLeft(), parent);
+       } else {
+//           changeParent = toDel;
+           return getLeftest(toDel.getRight(), parent);
+       }
     }
 
-    static Node getRightest(Node node) {
-        if (node.getRight() != null) node = getRightest(node.getRight());
+    static Node getRightest(Node node, Node parent) {
+        if (node.getRight() != null) {
+            changeParent = node;
+            node = getRightest(node.getRight(), parent);
+        }
         return node;
     }
 
-    static Node getLeftest(Node node) {
-        if (node.getLeft() != null) node = getLeftest(node.getLeft());
+    static Node getLeftest(Node node, Node parent) {
+        if (node.getLeft() != null) {
+            changeParent = node;
+            parent = node;
+            node = getLeftest(node.getLeft(), parent);
+        }
         return node;
     }
 
