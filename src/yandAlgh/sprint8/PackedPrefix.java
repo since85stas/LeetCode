9dda21bcf7781a2605641e3ccd5d1e8e0b30a855
Note <seeyou85@gmail.com>
2021-06-02 11:59:16 +0300

    задача 1 решена

--- src/yandAlgh/sprint8/PackedPrefix.java
+++ src/yandAlgh/sprint8/PackedPrefix.java
@@ -1,9 +1,15 @@
 package yandAlgh.sprint8;
 
-import javax.rmi.ssl.SslRMIClientSocketFactory;
 import java.io.*;
 import java.util.*;
 
+/**
+ *
+ * <p>
+ * сложность распаковки линейная по идее. Сложность нахождения префикса O (m * n) n - число строк, m - мин размер строки
+  * <p>
+ * успешная посылка  https://contest.yandex.ru/contest/25597/run-report/51582252/
+ */
 public class PackedPrefix {
 
     public static void main(String[] args) throws IOException {
@@ -11,60 +17,38 @@ public class PackedPrefix {
         int n = Integer.parseInt(reader.readLine());
         String[] unpacked = new String[n];
         for (int i = 0; i < n; i++) {
-            unpacked[i] = unpack(reader.readLine());
+            unpacked[i] = unpack(new StringBuilder(reader.readLine()));
         }
 
-
-
-//        for (int i = 0; i < pack.length; i++) {
-//            unpacked[i] = unpack(pack[i]);
-//        }
-
-//        Trie trie = new Trie();
-//        for (int i = 0; i < unpacked.length; i++) {
-//            trie.put(unpacked[i],i);
-//        }
         String longest = longestCommonPrefix2(unpacked);
         System.out.println(longest);
     }
 
-    private static String unpack(String packed) {
+    private static String unpack(StringBuilder packed) {
         StringBuilder unpacked = new StringBuilder();
-        StringBuilder cash = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
+//        StringBuilder cash = new StringBuilder();
+        Stack<StringBuilder> stack = new Stack<>();
+        Stack<Integer> mult = new Stack<>();
+        stack.push(unpacked);
         for (int i = 0; i < packed.length(); i++) {
-            if (packed.charAt(i) == '[') {
-                if (stack.size() == 0 )cash = new StringBuilder();
-            } else if (packed.charAt(i) == ']') {
-                String mul = cash.toString();
-                int m = stack.pop();
-                for (int j = 0; j < m-1; j++) {
-                    cash.append(mul);
+            char elem  =packed.charAt(i);
+            if (elem == '[') {
+                StringBuilder sub = new StringBuilder();
+                stack.push(sub);
+            } else if (elem == ']') {
+                int m = mult.pop();
+                StringBuilder ss = stack.pop();
+                for (int j = 0; j < m; j++) {
+                    stack.peek().append(ss);
                 }
-                if (stack.size() == 0) {
-                    unpacked.append(cash);
-                    cash = new StringBuilder();
-                }
-            } else if (packed.charAt(i) >= '1'  && packed.charAt(i) <= '9' ) {
-                int mult = Integer.parseInt(String.valueOf(packed.charAt(i)));
-                stack.push(mult);
+            } else if (elem >= '1'  && elem <= '9' ) {
+//                stack.peek().append(elem);
+                mult.push(Character.getNumericValue((elem)));
             } else {
-                if (stack.size() == 0)unpacked.append(packed.charAt(i));
-                else cash.append(packed.charAt(i));
+                stack.peek().append(elem);
             }
         }
         return unpacked.toString();
-    }
-
-    public static String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++)
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) return "";
-            }
-        return prefix;
     }
 
     public static String longestCommonPrefix2(String[] strs) {
