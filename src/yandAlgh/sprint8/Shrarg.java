88d93aa3dd4726a420d76e2e3b7759f38b28b4af
Note <seeyou85@gmail.com>
2021-06-03 09:32:58 +0300

    задача 2 решена

--- src/yandAlgh/sprint8/Shrarg.java
+++ src/yandAlgh/sprint8/Shrarg.java
@@ -1,18 +1,16 @@
 package yandAlgh.sprint8;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Shrarg {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("input.txt"))));
         StringBuilder sb = new StringBuilder(reader.readLine());
-        sb.append("#");
+//        sb.append("#");
         String s = sb.toString();
+
         StringTokenizer tokenizerStr = new StringTokenizer(reader.readLine());
         int n = Integer.parseInt(tokenizerStr.nextToken());
 
@@ -25,55 +23,54 @@ public class Shrarg {
             trie.put(tokenizerStr.nextToken(), i);
         }
 
-
-
-//        for (int i = 0; i < words.length; i++) {
-//            trie.put(words[i], i);
-//        }
-//        System.out.println(trie.contains("a"));
-//        System.out.println(trie.contains("pass"));
-
-//        Iterable<String >pref =  trie.keysWithPrefix("e");
         int start = 0;
         boolean check = false;
         boolean isOk = true;
-//        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-//            if (i == s.length() -1) break;
 
-            String pref = s.substring(start,i+1);
-            if (pref.equals("#")) break;
-            Queue<String > qu =  trie.keysWithPrefix(pref);
-            if (qu.size() == 0) {
-                if (check) {
-                    isOk = false;
-                    break;
-                } else {
-                    check = true;
-                    i = i-2;
-                }
-            } else {
-                if (check) {
-                    if (trie.contains(pref)) {
-//                        builder.append(pref);
-                        start = i + 1;
-                        check = false;
-                    } else {
-                        isOk = false;
-                        break;
-                    }
-                }
-            }
-        }
-        if (isOk) System.out.println("YES");
-        else System.out.println("NO");
+        List<String> result = new ArrayList<>();
+
+        trie.testWord(s,0, new String(), result);
+
+        if (result.size() == 0) System.out.println("NO");
+        else System.out.println("YES");
+
+//        for (int i = 0; i < s.length(); i++) {
+//
+//            String pref = s.substring(start,i+1);
+//            if (pref.equals("#")) break;
+////            Queue<String > qu =  trie.keysWithPrefix(pref);
+//            if (!trie.containsP(pref)) {
+//                if (check) {
+//                    isOk = false;
+//                    break;
+//                } else {
+//                    check = true;
+//                    i = i-2;
+//                }
+//            } else {
+//                if (check) {
+//                    if (trie.contains(pref)) {
+////                        builder.append(pref);
+//                        start = i + 1;
+//                        check = false;
+//                    } else {
+//                        isOk = false;
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//        if (isOk) System.out.println("YES");
+//        else System.out.println("NO");
 //        System.out.println();
     }
 
 
+
+
     static class Trie {
 
-        private static final int R = 150;
+        private static final int R = 256;
         private Node root = new Node();
 
         public void put(String key, int val)
@@ -126,6 +123,38 @@ public class Shrarg {
             Node x = get(root, prefix, 0);
             collect(x, prefix, queue);
             return queue;
+        }
+
+        public boolean containsP(String key)
+        { return containshPrefix(key) != null; }
+
+        public Node containshPrefix(String prefix)
+        {
+            Node x = get(root, prefix, 0);
+            if (x == null) return null;
+            else return  x;
+        }
+
+        public void testWord(String word, int index, String s, List<String> result)
+        {
+            Node curr = root;
+
+            for(int i=index; i<word.length();i++)
+            {
+                if(curr.next[word.charAt(i)]==null)
+                    return;
+                if(curr.next[word.charAt(i)].value !=null)
+                {
+                    if(i==word.length()-1)
+                    {
+                        result.add(new String(s) + word.substring(index, i+1));
+                        return;
+                    }
+
+                    testWord(word, i+1, s + word.substring(index, i+1) + " ", result);
+                }
+                curr = curr.next[word.charAt(i)];
+            }
         }
 
     }
